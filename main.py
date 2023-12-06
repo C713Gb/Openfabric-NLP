035478fe446f4e8524f81538141938f232d78ac4
C713Gb <banerjeec713@gmail.com>
2023-12-06 21:13:35 +0530

    Created a science chatbot with a sample Science data.txt

--- main.py
+++ main.py
@@ -1,66 +1,76 @@
 import os
 import warnings
-import tensorflow as tf
 from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-from transformers import pipeline, AutoTokenizer, TFAutoModelForQuestionAnswering
-
-tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
-model = TFAutoModelForQuestionAnswering.from_pretrained('allenai/scibert_scivocab_uncased', from_pt=True)
-
-# Initialize the model once (outside the function to avoid reloading it on each call)
-science_qa_pipeline = pipeline("question-answering", model="deepset/roberta-base-squad2",
-                               tokenizer="deepset/roberta-base-squad2")
-
-
-############################################################
-# Callback function called on update config
-############################################################
-def config(configuration: ConfigClass):
-    # TODO Add code here
-    pass
+# Download NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
-############################################################
-# Callback function called on each execution pass
-############################################################
+# Read science documents from a text file
+def read_documents_from_file(file_path):
+    with open(file_path, 'r') as file:
+        documents = file.readlines()
+    return [doc.strip() for doc in documents]
+
+
+# Preprocess the documents
+def preprocess_text(text):
+    stop_words = set(stopwords.words('english'))
+    ps = PorterStemmer()
+    words = word_tokenize(text.lower())
+    words = [ps.stem(word) for word in words if word.isalnum() and word not in stop_words]
+    return ' '.join(words)
+
+
+# Function to get the most relevant document for a given query
+def get_most_similar_document(query, documents, vectorizer, tfidf_matrix):
+    query = preprocess_text(query)
+    query_vector = vectorizer.transform([query])
+    similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
+    most_similar_index = similarities.argmax()
+    return documents[most_similar_index]
+
+
+# Sample science-related documents
+file_path = os.getcwd() + '/data.txt'
+
+science_documents = read_documents_from_file(file_path)
+processed_documents = [preprocess_text(doc) for doc in science_documents]
+
+# Vectorize the documents using TF-IDF
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(processed_documents)
+
+
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
     for text in request.text:
-        print("text ==> ", text)
-        response = process_science_query(text)  # process_science_query is a placeholder for your NLP logic
+        response = get_most_similar_document(text, science_documents, vectorizer, tfidf_matrix)
         output.append(response)
 
-    formatted_output = '\n'.join(output)
-    response_text = SimpleText(dict(text=formatted_output))
-
-    # Print the content of the SimpleText object for debugging
-    print("Response content:", response_text.text)
-
-    return response_text
+    return SimpleText(dict(text=output))
 
 
-def process_science_query(query):
-    inputs = tokenizer.encode_plus(query, add_special_tokens=True, return_tensors="tf")
-    input_ids = inputs["input_ids"].numpy()[0]
+if __name__ == "__main__":
+    print("Science Chatbot: Hi! I'm here to answer your science-related questions. Type 'exit' to stop.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            print("Science Chatbot: Goodbye!")
+            break
 
-    outputs = model(inputs)
-    answer_start_scores, answer_end_scores = outputs.start_logits, outputs.end_logits
-
-    answer_start = tf.argmax(answer_start_scores, axis=1).numpy()[0]
-    answer_end = (tf.argmax(answer_end_scores, axis=1) + 1).numpy()[0]
-
-    # Debugging: Print answer start and end positions
-    print("Answer start:", answer_start)
-    print("Answer end:", answer_end)
-
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-
-    # Debugging: Print the extracted answer
-    print("Extracted answer:", answer)
-
-    return answer
+        mock_request = SimpleText(dict(text=[user_input]))
+        response = execute(mock_request, None)
+        for resp in response.text:
+            print(resp)
